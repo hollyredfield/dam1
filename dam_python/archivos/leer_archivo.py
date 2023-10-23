e21e65e2ba95515f7d2475b9e270bbdb38aa5078
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-10-23 08:13:10 +0000

    leer_archivo

--- dam_python/archivos/leer_archivo.py
+++ dam_python/archivos/leer_archivo.py
@@ -1,5 +1,27 @@
 #script para leer archivos de texto.
-with open('el_archivo.txt', 'r') as archivo:
+with open('mi_archivo.txt', 'r') as archivo:
     contenido = archivo.read()
     print (contenido)
 archivo.close()
+#leer las listas de un archivo
+""" 
+lista = []  # Inicializa la lista
+with open('frutas.txt', 'r') as archivo:
+    frutas = archivo.readlines()
+    frutas = [fruta.strip() for fruta in frutas]
+    for fruta in frutas:
+        lista.append(fruta)
+    print(frutas)
+    print('\nEsta es la lista llamada lista:', lista)
+archivo.close()  # Cierra el archivo correctamente
+ """
+ lista = [] 
+ frutas = []    # Inicializa la lista
+with open('frutas.txt', 'r') as archivo:
+    frutas = archivo.readlines()
+    #frutas = [fruta.strip() for fruta in frutas]
+    for fruta in frutas:
+        lista.append(fruta.strip())
+    #print(frutas)
+    print('\nEsta es la lista llamada lista:', lista)
+archivo.close()  # Cierra el archivo correctamente
