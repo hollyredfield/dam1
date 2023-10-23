58856ac7ca422b7e96326fcd5b9de3d7422251f4
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-10-23 07:39:30 +0000

    archivos

--- dam_python/archivos/escribirarchivos.py
+++ dam_python/archivos/escribirarchivos.py
@@ -1,5 +1,5 @@
 archivo = open ("mi_archivo.txt", "w")
-archivo.write("Hola mundo")
+archivo.write("Hola mundo, ¿qué tal?")
 archivo.close
 
 #crear un archivo de texto y escribir dentro de una lista
