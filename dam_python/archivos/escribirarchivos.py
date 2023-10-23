32bcae971d524ba5367209cbefc9137b3d539110
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-10-23 07:46:07 +0000

    save

--- dam_python/archivos/escribirarchivos.py
+++ dam_python/archivos/escribirarchivos.py
@@ -2,4 +2,8 @@ archivo = open ("mi_archivo.txt", "w")
 archivo.write("Hola mundo, ¿qué tal?")
 archivo.close
 
-#crear un archivo de texto y escribir dentro de una lista
+#crear un archivo de texto y escribir dentro de una lista:
+mi_lista = ['manzana', 'plátano','cereza']
+with open ("frutas.txt", "w") as archivo:
+    for fruta in mi_lista:
+        archivo.write(fruta +'\n')
