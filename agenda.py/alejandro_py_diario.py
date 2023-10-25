4e6c105dcc409a0253179583e720e600f83e2622
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-10-25 12:09:24 +0000

    save

---
+++ agenda.py/alejandro_py_diario.py
@@ -0,0 +1,18 @@
+
+                
+                
+                
+while True: #Funcion Principal
+    opcion = menu()
+    
+    if opcion == 1:
+        diario = anadir_diario()
+    elif opcion == 2:
+        ver_diario()
+    elif opcion == 3:
+        diario = eliminar_diario()
+    elif opcion == 4:
+        print(f"\nHASTA LA PROXIMA")
+        break
+    else:
+        print(f"\nIntentalo de nuevo")
