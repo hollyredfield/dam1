644cf2d71e352162ec1c261f0805aac057975d77
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-10-26 14:49:40 +0000

    save

---
+++ gestion_biblioteca/biblioteca_modulo.py
@@ -0,0 +1,25 @@
+""" 
+Gestión de Biblioteca
+Objetivo: Desarrollar un programa que permita gestionar los libros de una biblioteca.
+Funcionalidades:
+
+Agregar un libro: Cada libro debe tener un título, autor, año de publicación y género.
+
+Visualizar todos los libros: Mostrar una lista de todos los libros en la biblioteca.
+
+Buscar un libro por título o autor: Permitir al usuario buscar un libro específico.
+
+Eliminar un libro: Permitir al usuario eliminar un libro de la biblioteca.
+
+Prestar un libro: Registrar cuándo un libro ha sido prestado y a quién.
+
+Devolver un libro: Registrar cuándo un libro ha sido devuelto.
+
+Visualizar libros prestados: Mostrar una lista de todos los libros que están prestados y quién los tiene.
+Especificaciones adicionales:
+
+Los libros y los préstamos deben ser almacenados en un fichero de texto.
+
+El programa debe manejar posibles errores, como intentar prestar un libro que ya ha sido prestado 
+o buscar un libro que no existe en la biblioteca.
+ """
