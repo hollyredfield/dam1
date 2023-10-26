01f963c736fdf39fe027a9751880b06a14875e6e
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-10-26 15:37:18 +0000

    save

--- gestion_biblioteca/biblioteca_modulo.py
+++ gestion_biblioteca/biblioteca_modulo.py
@@ -26,20 +26,24 @@ o buscar un libro que no existe en la biblioteca.
  #ha de haber dos ficheros de textos, uno de los libros y otro de los prestados
 def agregarunlibro():#Agregar un libro: Cada libro debe tener un título, autor, año de publicación y género
     try:
-        libro = input("Dime el libro que quieras añadir a la biblioteca: ")
+        libro = input("Dime el título del libro que quieras añadir a la biblioteca: ")
+        autor = input("dime")
         with open("libros.txt", "a") as file:
             file.write(libro + "\n")
             print(f"{libro} agregado correctamente")
     except FileNotFoundError:
         print("Error")
     return        
-agregarunlibro()
-"""         
-def visualizarlibro():#Visualizar todos los libros: Mostrar una lista de todos los libros en la biblioteca
-def buscarlibro():#Buscar un libro por título o autor: Permitir al usuario buscar un libro específico
-def eliminarlibro():#Eliminar un libro: Permitir al usuario eliminar un libro de la biblioteca
-def prestarlibro():#Prestar un libro: Registrar cuándo un libro ha sido prestado y a quién debe estar en un fichero de prestar libros
-def devolverlibro():#Devolver un libro: Registrar cuándo un libro ha sido devuelto
-def visualizarlibrosprestados():#Mostrar una lista de todos los libros que están prestados y quién los tiene. Debe estar en un fichero de prestar libros 
-"""
+
+      
+#def visualizarlibro():#Visualizar todos los libros: Mostrar una lista de todos los libros en la biblioteca
+    #try:
+        #with open("libros.txt", "r") as file:
+            #read = file.readlines()
+#def buscarlibro():#Buscar un libro por título o autor: Permitir al usuario buscar un libro específico
+#def eliminarlibro():#Eliminar un libro: Permitir al usuario eliminar un libro de la biblioteca
+#def prestarlibro():#Prestar un libro: Registrar cuándo un libro ha sido prestado y a quién debe estar en un fichero de prestar libros
+#def devolverlibro():#Devolver un libro: Registrar cuándo un libro ha sido devuelto
+#def visualizarlibrosprestados():#Mostrar una lista de todos los libros que están prestados y quién los tiene. Debe estar en un fichero de prestar libros 
+
     
