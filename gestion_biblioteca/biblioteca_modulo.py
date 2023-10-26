ee88b8e64937fb3f598761ddd6f3acefbc6c9f30
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-10-26 15:29:50 +0000

    save

--- gestion_biblioteca/biblioteca_modulo.py
+++ gestion_biblioteca/biblioteca_modulo.py
@@ -33,11 +33,13 @@ def agregarunlibro():#Agregar un libro: Cada libro debe tener un título, autor,
     except FileNotFoundError:
         print("Error")
     return        
+agregarunlibro()
 """         
 def visualizarlibro():#Visualizar todos los libros: Mostrar una lista de todos los libros en la biblioteca
 def buscarlibro():#Buscar un libro por título o autor: Permitir al usuario buscar un libro específico
 def eliminarlibro():#Eliminar un libro: Permitir al usuario eliminar un libro de la biblioteca
 def prestarlibro():#Prestar un libro: Registrar cuándo un libro ha sido prestado y a quién debe estar en un fichero de prestar libros
 def devolverlibro():#Devolver un libro: Registrar cuándo un libro ha sido devuelto
-def visualizarlibrosprestados():#Mostrar una lista de todos los libros que están prestados y quién los tiene. Debe estar en un fichero de prestar libros """
+def visualizarlibrosprestados():#Mostrar una lista de todos los libros que están prestados y quién los tiene. Debe estar en un fichero de prestar libros 
+"""
     
