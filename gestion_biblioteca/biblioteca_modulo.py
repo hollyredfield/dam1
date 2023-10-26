df2e8890bbc8d3e50f9d514bb9cc676b1968149c
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-10-26 15:13:14 +0000

    save

--- gestion_biblioteca/biblioteca_modulo.py
+++ gestion_biblioteca/biblioteca_modulo.py
@@ -23,3 +23,12 @@ Los libros y los préstamos deben ser almacenados en un fichero de texto.
 El programa debe manejar posibles errores, como intentar prestar un libro que ya ha sido prestado 
 o buscar un libro que no existe en la biblioteca.
  """
+ #ha de haber dos ficheros de textos, uno de los libros y otro de los prestados
+ def agregarunlibro(): #Agregar un libro: Cada libro debe tener un título, autor, año de publicación y género
+def visualizarlibro():#Visualizar todos los libros: Mostrar una lista de todos los libros en la biblioteca
+def buscarlibro():#Buscar un libro por título o autor: Permitir al usuario buscar un libro específico
+def eliminarlibro():#Eliminar un libro: Permitir al usuario eliminar un libro de la biblioteca
+def prestarlibro():#Prestar un libro: Registrar cuándo un libro ha sido prestado y a quién debe estar en un fichero de prestar libros
+def devolverlibro():#Devolver un libro: Registrar cuándo un libro ha sido devuelto
+def visualizarlibrosprestados():#Mostrar una lista de todos los libros que están prestados y quién los tiene. Debe estar en un fichero de prestar libros
+    
