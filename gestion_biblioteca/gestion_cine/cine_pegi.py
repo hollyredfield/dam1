0e3c130d4fa2d876fab79ea76b9fbde0559033b2
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-10-30 08:29:26 +0000

    save

---
+++ gestion_biblioteca/gestion_cine/cine_pegi.py
@@ -0,0 +1,35 @@
+"""
+Gestión de un Cine
+Desarrollar un programa que permita gestionar las películas y las funciones en un cine.
+Funcionalidades:
+Agregar una película: Cada película debe tener un título, director, duración (en minutos) y clasificación 
+(ej. PG, PG-13, PG-18, PG+18).
+
+Visualizar todas las películas: Mostrar una lista de todas las películas disponibles en el cine.
+
+Buscar una película por título o director: Permitir al usuario buscar una película específica.
+
+Eliminar una película: Permitir al usuario eliminar una película de la lista.
+
+Especificaciones adicionales:
+Las películas deben ser almacenadas en un fichero de texto.
+
+El programa debe manejar posibles errores.
+
+Consejos:
+Puedes usar una estructura de datos, como una lista para almacenar la información de las películas.
+Considera usar funciones separadas para cada funcionalidad para mantener el código organizado.
+Antes de escribir el código, es útil planificar cómo estructurarás la información en el fichero de texto y 
+cómo la leerás y escribirás.
+ """
+def agregarpelicula():
+
+def visualizarpelicula():
+
+def buscarpelicula():
+    
+def eliminarpelicula():
+
+def menu():
+    
+    
