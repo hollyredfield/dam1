374a4f2d3c3043d6da53ebb58143180e513db860
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-11-06 09:03:03 +0000

    save

---
+++ Persistencia datos_en _bases_de_datos/persistencia_mysql.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+
+    try:
+        # Establecer la conexión
+        conexion = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='',
+            database='supermercado  '
+        )
+        print("Conexión exitosa.")
+    except mysql.connector.Error as e:
+        print("Error al conectar a la base de datos: ", e)
