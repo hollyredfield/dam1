9df8259863ebb944ad8fbda21c25af078271d56b
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-11-06 11:28:57 +0000

    save

--- Persistencia datos_en _bases_de_datos/persistencia_mysql.py
+++ Persistencia datos_en _bases_de_datos/persistencia_mysql.py
@@ -1,14 +1,26 @@
-import mysql.connector
-
+    import mysql.connector 
+#dkd
     try:
         # Establecer la conexión
         conexion = mysql.connector.connect(
             host='localhost',
             user='root',
-            password='',
-            database='supermercado  '
+            password='curso',
+            database='supermercado'
         )
         print("Conexión exitosa.")
+        #código para crear registros
+        cursor = conexion.cursor()
+        query = "INSERT INTO producto (nombre, idcategoria,idproducto) VALUES (%s,%s,%s)"
+        valores = ["Fardastopa Radioactiva", 1, 200]
+        cursor.execute(query, valores)
+        conexion.commit()
+        print("Usuario añadido con éxito.")
+        cursor.close()
+        conexion.close()
+
     except mysql.connector.Error as e:
         print("Error al conectar a la base de datos: ", e)
-#dwdwwwd
+    
+        #dcjbsdfknjjfjf
+        
