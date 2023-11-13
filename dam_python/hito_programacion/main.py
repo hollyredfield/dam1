0cbc3478ebd6fa98d72d9832e472e7f93995d947
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-11-13 20:08:15 +0000

    save

--- dam_python/hito_programacion/main.py
+++ dam_python/hito_programacion/main.py
@@ -1,32 +1,32 @@
 from menu import menu
+from clientes import *
+from articulo import *
+from compras import *
 
-
-clientes = []
-articulos = []
 
 while True:
     option = menu()
     if option == 1:
-        registrar_cliente(clientes)
-    elif option == 11:
-        print("Chao, Pescao.")
-        break
+        anadircliente = registrarcliente(clientes)
     elif option == 2:
-        ver_clientes()
+        verclientes()
     elif option == 3:
-        buscar_cliente()
+        buscarclientes()
     elif option == 4:
-        eliminar_cliente(clientes)
+        eliminar = eliminarcliente(clientes)
     elif option == 8:
-        registrar_articulo(articulos)
+        anadir = registrodearticulo(articulos)
     elif option == 5:
-        ver_articulos()
+        verarticulos()
     elif option == 6: 
-        realizar_compra()
+        realizarcompra()
     elif option == 9:
         eliminar_articulo(articulos)
     elif option == 10:
         buscar_articulo()
+    elif option == 11:
+        print("Chao, Pescao.")
+        break
 
 
 
