889e9609b5f3fceb592d37c151f112dbbe1e4195
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-11-13 20:15:37 +0000

    save

--- dam_python/hito_programacion/main.py
+++ dam_python/hito_programacion/main.py
@@ -39,30 +39,6 @@ while True:
 
 
 
-
-while True:
-    option = menu()
-    if option == 1:
-        anadircliente = registrarcliente(clientes)
-    elif option == 11:
-        print("Chao, Pescao.")
-        break
-    elif option == 2:
-        verclientes()
-    elif option ==3:
-        buscarclientes()
-    elif option == 4:
-        eliminar= eliminarcliente(clientes)
-    elif option == 8:
-        anadir = registrodearticulo(articulos)
-    elif option == 5:
-        verarticulos()
-    elif option ==6: 
-        realizarcompra()
-    elif option == 9:
-        eliminar_articulos = eliminar_articulo(articulos)    
-    elif option == 10:
-        buscar_articulo()  
 """ 
         elif option ==7:
         seguimientodecompra() 
