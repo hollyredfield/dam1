8f16d14870402da6d35e94359fc7c921b0cb79fc
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-11-13 08:40:26 +0000

    save

--- dam_python/hito_programacion/main.py
+++ dam_python/hito_programacion/main.py
@@ -1,8 +1,12 @@
 def menu():
-    print ("1.- Bienvenido a la app.")
-    print ("2.- Bienvenido a la app.")
-    print ("3.- Bienvenido a la app.")
-    print ("4.- Bienvenido a la app.")
+    print("1.- Bienvenido a la app.")
+    print("2.- Registrar Cliente")
+    print("3.- Ver clientes")
+    print("4.- Buscar Clientes")
+    print("5.- Eliminar Cliente ")
+    print("6.- ")
+
+ 
 
 
 
