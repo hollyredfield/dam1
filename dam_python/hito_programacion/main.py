e3a7cb3b9201ee2618e8933266eec4438b53f720
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-11-13 08:47:25 +0000

    save

--- dam_python/hito_programacion/main.py
+++ dam_python/hito_programacion/main.py
@@ -4,9 +4,15 @@ def menu():
     print("3.- Ver clientes")
     print("4.- Buscar Clientes")
     print("5.- Eliminar Cliente ")
-    print("6.- ")
+    print("6.- Ver artículos ")
+    print("7.- Realizar compra")
+    print("8.- Seguimiento de compra")
+    print("9.- Registro de artículo")
+    print("10.- Salir")
 
- 
+
+
+
 
 
 
