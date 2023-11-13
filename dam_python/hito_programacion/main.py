54184b5825dddf0c946b434c310677bae3073d34
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-11-13 08:37:25 +0000

    save

---
+++ dam_python/hito_programacion/main.py
@@ -0,0 +1,9 @@
+def menu():
+    print ("1.- Bienvenido a la app.")
+    print ("2.- Bienvenido a la app.")
+    print ("3.- Bienvenido a la app.")
+    print ("4.- Bienvenido a la app.")
+
+
+
+    
