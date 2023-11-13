7e82a28b526088078530c54272bf602fa266de7f
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-11-13 09:47:36 +0000

    save

--- dam_python/hito_programacion/main.py
+++ dam_python/hito_programacion/main.py
@@ -8,36 +8,53 @@ def menu():
     print("6.- Realizar compra")
     print("7.- Seguimiento de compra")
     print("8.- Registro de artículo")
-    print("9.- Salir")
+    print("9. Eliminar Artículo")
+    print("10. Buscar Artículo")
+    print("11.- Salir")
     option = input("¿Qué quieres hacer?:")
     return (int(option))
 menu()
 
 def registrarcliente():  
-def verclientes(): 
+    cliente = input ("Dime tu nombre: ")
+    
+""" def verclientes(): 
 def buscarclientes():
-def eliminarclientes():
+def eliminarcliente():
 def verarticulos():
 def realizarcompra():
 def seguimientodecompra():
 def registrodearticulo():
+def eliminar_articulo():
+def buscar_articulo():
+ """
 
 while True:
     option = menu()
     if option == 1:
+        registrarcliente()
+"""     elif option == 2:
         verclientes()
-    elif option ==2:
-        verclientes()
-    elif option == 3:
-        eliminarclientes()
+    elif option ==3:
+        buscarclientes()
     elif option == 4:
+        eliminarcliente()
+    elif option == 5:
         verarticulos()
-    elif option ==5: 
+    elif option ==6: 
         realizarcompra()
-    elif option ==6:
+    elif option ==7:
         seguimientodecompra()
-    elif option == 7:
-        registrode
+    elif option == 8:
+        registrodearticulo()
+    elif option == 9:
+        eliminar_articulo()
+    elif option == 10:
+        buscar_articulo() """
+    else:
+        if option == 11:
+            print("Chao, Pescao.")
+            break
 
 
 
