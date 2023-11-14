e4c3ee455b5aded8c1a64f6a2763927b04c29316
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-11-14 13:05:35 +0000

    save

--- dam_python/hito_programacion/clientes.py
+++ dam_python/hito_programacion/clientes.py
@@ -17,10 +17,7 @@ def login():
                 print("Error: Usuario no encontrado.")
     except FileNotFoundError:
         print(f"El {usuario} escrito no existe.")
-
-
-
-                
+    return               
 clientes = []
 
 def registrarcliente(clientes):  
@@ -28,10 +25,8 @@ def registrarcliente(clientes):
     apellido = input("Apellidos: ")
     numero = input("Dime tu número: ")
     dni = input("DNI: ")
-    
     # Generar un ID único para el nuevo cliente
     nuevo_id = len(clientes) + 1
-    
     try:
         with open("clientes.txt", "a") as file:
             file.write(f"{nuevo_id},{nombre},{apellido},{numero},{dni}\n")
@@ -41,12 +36,6 @@ def registrarcliente(clientes):
     except FileNotFoundError:
         print("Error al agregar el cliente.")
     return clientes
-
-
-
-
-    
-    
 def verclientes(): 
     try:
         with open("clientes.txt", "r") as file:
@@ -65,23 +54,15 @@ def verclientes():
     except FileNotFoundError:
         print("Error al ver los clientes")
     return
-
-
-    
-    
-    
-
 def buscarclientes():
     criterio_busqueda = input("Selecciona el criterio de búsqueda (nombre, DNI, número o teléfono): ").lower()
     valor_buscado = input(f"Dime el {criterio_busqueda} del cliente que quieres buscar: ")
     encontrado = False
-
     try:
         with open("clientes.txt", "r") as file:
             lines = file.readlines()
             for line in lines:
                 cliente = line.strip().split(',')
-                
                 # Verificar que haya suficientes elementos en la lista
                 if len(cliente) >= 4:
                     if criterio_busqueda == "nombre" and valor_buscado in cliente[0]:
@@ -92,31 +73,24 @@ def buscarclientes():
                         encontrado = True
                     elif criterio_busqueda == "teléfono" and valor_buscado == cliente[1]:
                         encontrado = True
-
                     if encontrado:
                         print(f"Cliente encontrado - Nombre: {cliente[0]}, Apellido: {cliente[1]}, Número: {cliente[2]}, DNI: {cliente[3]}")
                         encontrado = False
-
             if not encontrado:
                 print(f"No se encontró ningún cliente con {criterio_busqueda} igual a {valor_buscado}")
-
     except FileNotFoundError:
         print("Error al buscar el cliente")
     return
-
 def eliminarcliente(clientes):
     verclientes()
     dni_a_eliminar = input("Dime el DNI del cliente que quieres eliminar: ")
     encontrado = False
-
     try:
         with open("clientes.txt", "r") as file:
             lines = file.readlines()
-
         with open("clientes.txt", "w") as file:
             for line in lines:
                 cliente = line.strip().split(',')
-
                 # Verificar que haya suficientes elementos en la lista
                 if len(cliente) >= 4:
                     if dni_a_eliminar == cliente[3]:
@@ -124,10 +98,8 @@ def eliminarcliente(clientes):
                         print(f"El cliente con DNI {dni_a_eliminar} ha sido eliminado correctamente.")
                     else:
                         file.write(line)
-
         if not encontrado:
             print(f"No se encontró ningún cliente con DNI {dni_a_eliminar}")
-
     except FileNotFoundError:
         print("Error al eliminar el cliente")
     return clientes
