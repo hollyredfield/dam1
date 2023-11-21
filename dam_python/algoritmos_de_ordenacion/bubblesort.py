89139110874f9d2f78a947170cf618a728bd3350
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-11-21 12:34:59 +0000

    Refactored sorting algorithms

--- dam_python/algoritmos_de_ordenacion/bubblesort.py
+++ dam_python/algoritmos_de_ordenacion/bubblesort.py
@@ -1,16 +1,12 @@
+Ejemplodelista: [64, 34, 25, 12, 22, 11, 90]
 def bubbleSort(arr):
-    n = len(arr)  # Obtener la longitud del array
-
-    # Recorrer todos los elementos del array
+    n = len(arr)  # Obtenemos la longitud del array
+    # Recorremos todos los elementos del array
     for i in range(n):
-
         # Los últimos i elementos ya están en su lugar
         for j in range(0, n-i-1):
-
-            # Recorrer el array desde 0 hasta n-i-1
-            # Intercambiar si el elemento encontrado es mayor que el siguiente
+            # Recorremos el array desde 0 hasta n-i-1
+            # Intercambiamos si el elemento encontrado es mayor que el siguiente
             if arr[j] > arr[j+1] :
-                arr[j], arr[j+1] = arr[j+1], arr[j]  # Intercambiar
-
-    return arr  # Devolver el array ordenado
-#ordena por número pares de izquierda a derecha, y va comparando de nuevo los números desde el principio, si el primero es mayor que el seguno, les da la vuelta hasta dejar los números mayores al final.
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr  # Devolvemos el array ordenado
--- dam_python/algoritmos_de_ordenacion/quicksort.py
+++ dam_python/algoritmos_de_ordenacion/quicksort.py
@@ -1,22 +1,22 @@
+# Ejemplo de lista: [10, 7, 8, 9, 1, 5]
 def partition(arr, low, high):
     i = (low-1)  # Índice del elemento más pequeño
     pivot = arr[high]  # Pivote
-
     for j in range(low, high):
         # Si el elemento actual es menor o igual al pivote
         if arr[j] <= pivot:
-            i = i+1  # Incrementar el índice del elemento más pequeño
-            arr[i], arr[j] = arr[j], arr[i]  # Intercambiar
-
-    arr[i+1], arr[high] = arr[high], arr[i+1]  # Colocar el pivote en su lugar correcto
-    return (i+1)  # Devolver el índice del pivote
+            i = i+1  # Incrementamos el índice del elemento más pequeño
+            arr[i], arr[j] = arr[j], arr[i]  # Y lo intercambiamos con el elemento en j
+    arr[i+1], arr[high] = arr[high], arr[i+1]  # Intercambiamos el pivote con el elemento en i+1
+    return (i+1)  # Devolvemos la posición del pivote
 
 def quickSort(arr, low, high):
     if len(arr) == 1:
-        return arr  # Si el array tiene un solo elemento, ya está ordenado
+        return arr
     if low < high:
-        pi = partition(arr, low, high)  # Índice del pivote
-
-        # Ordenar los elementos a la izquierda y a la derecha del pivote
+        # pi es la posición del pivote en el array ordenado
+        pi = partition(arr, low, high)
+        # Ordenamos los elementos antes y después del pivote
         quickSort(arr, low, pi-1)
-        quickSort(arr, pi+1, high) 
+        quickSort(arr, pi+1, high)
+    return arr  # Devolvemos el array ordenado
