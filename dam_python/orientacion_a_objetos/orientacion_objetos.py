6150e5e385fd680215f25b71c269878372b13529
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-11-27 11:59:02 +0000

    Add new classes and methods

--- dam_python/orientacion_a_objetos/orientacion_objetos.py
+++ dam_python/orientacion_a_objetos/orientacion_objetos.py
@@ -12,3 +12,28 @@ class Libro:
     def leer(self):
         print(f"Leyendo {self.paginas} páginas de {self.titulo}.")
 mi_libro = Libro("Cien Años de Soledad", "Gabriel García Márquez", 417)
+mi_libro.abrir()
+mi_libro.leer()
+class Persona:
+    """
+    Clase que representa a una persona.
+    """
+
+    especie = 'Homo sapiens'  # Atributo de clase
+
+
+    def __init__(self, nombre, edad):
+        self.nombre = nombre    # Atributo de instancia
+        self.edad = edad        # Atributo de instancia
+
+
+    def presentarse(self):
+        """
+        Método que imprime un mensaje de presentación de la persona.
+        """
+        print(f"Hola, mi nombre es {self.nombre} y tengo {self.edad} años.")
+        #Instanciar un objeto de la clase persona:
+empleado1 = Persona("Pepe", 24)
+empleado2 = Persona("Juan",32)
+empleado1.presentarse()
+empleado2.presentarse()
