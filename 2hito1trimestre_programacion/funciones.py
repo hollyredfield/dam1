acb47da0d35d9d40416aa6c580b1df11440b94b5
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-11-27 10:04:15 +0000

    Fix bug in agenda.py/alejandro_py_diario.py

---
+++ 2hito1trimestre_programacion/funciones.py
@@ -0,0 +1,26 @@
+""" 
+e que:
+    1) Generar de forma aleatoria un vector de 10 elementos enteros, los números aleatorios
+serán del 1 al 10 y se tiene que controlar que los números no se repitan.
+2) Mostrar el vector antes de la ordenación.
+3) Aplicar el algoritmo de ordenación: se debe mostrar información por pantalla para indicar
+los distintos pasos que realiza el algoritmo.
+4) Mostrar el vector después de la ordenación.
+
+Los algoritmos de ordenación son:
+I) Ordenación de burbuja (Bubble Sort) o intercambio directo
+II) Ordenación Shell o por inserción
+Realiza la implementación python de los dos pseudocódigos realizados. El programa
+contemplará los cuatro puntos de las especificaciones.
+▪ Buscar información sobre la búsqueda dicotómica o binaria, explicarlo con un ejemplo e
+implementarlo (se puede implementar la versión recursiva o iterativa) en PSeint o mediante
+pseudocódigo. Analiza su eficiencia 
+""" 
+
+def generar_numeros_aleatorios():
+    import random
+    numeros = [] 
+    while len(numeros) <10:
+        numero = random.randint(1, 10)
+        numeros.append(numero)
+    print(numeros)
--- agenda.py/alejandro_py_diario.py
+++ agenda.py/alejandro_py_diario.py
@@ -9,10 +9,11 @@ while True: #Funcion Principal
         diario = anadir_diario()
     elif opcion == 2:
         ver_diario()
-    elif opcion == 3:
+    elif opcion == 3: 
         diario = eliminar_diario()
     elif opcion == 4:
         print(f"\nHASTA LA PROXIMA")
         break
     else:
         print(f"\nIntentalo de nuevo")
+                    
