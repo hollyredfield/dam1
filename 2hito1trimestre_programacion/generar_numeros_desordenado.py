e408df658ff21e75e0dc1fcb6e76c6fe49ed31e3
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-11-30 12:40:32 +0000

    Refactored code to generate and sort a list of random numbers

--- 2hito1trimestre_programacion/generar_numeros_desordenado.py
+++ 2hito1trimestre_programacion/generar_numeros_desordenado.py
@@ -1,23 +1,57 @@
-""" 
-e que:
-    1) Generar de forma aleatoria un vector de 10 elementos enteros, los números aleatorios
-serán del 1 al 10 y se tiene que controlar que los números no se repitan.
-2) Mostrar el vector antes de la ordenación.
-3) Aplicar el algoritmo de ordenación: se debe mostrar información por pantalla para indicar
-los distintos pasos que realiza el algoritmo.
-4) Mostrar el vector después de la ordenación.
 
-Los algoritmos de ordenación son:
-I) Ordenación de burbuja (Bubble Sort) o intercambio directo
-II) Ordenación Shell o por inserción
-Realiza la implementación python de los dos pseudocódigos realizados. El programa
-contemplará los cuatro puntos de las especificaciones.
-▪ Buscar información sobre la búsqueda dicotómica o binaria, explicarlo con un ejemplo e
-implementarlo (se puede implementar la versión recursiva o iterativa) en PSeint o mediante
-pseudocódigo. Analiza su eficiencia 
-""" 
+""" Pseudocódigo del Planteamiento:
 
+Generación de Números Aleatorios:
 
+Importar la biblioteca random.
+Inicializar una lista vacía llamada numeros para almacenar los números generados.
+Mientras la longitud de la lista numeros sea menor que 10:
+Generar un número aleatorio entre 1 y 10.
+Si el número no está en la lista, agregarlo.
+Creación de Lista Desordenada:
+
+Llamar a la función de generación de números aleatorios y almacenar el resultado en la variable numeros.
+Intentar abrir el archivo "listadesornedada.txt" en modo de escritura (append).
+Para cada número en la lista numeros, escribirlo en el archivo separado por una coma y un espacio.
+Imprimir la lista de números generados.
+En caso de un error de archivo no encontrado, imprimir un mensaje de error.
+Implementación de Bubble Sort:
+
+Definir una función llamada bubblesort que tome una lista de números como argumento.
+Calcular la longitud de la lista.
+Utilizar dos bucles for para comparar y ordenar los elementos de la lista mediante el algoritmo de ordenación de burbuja.
+Imprimir la lista después de cada intercambio.
+Devolver la lista ordenada.
+Ordenar Lista Utilizando Bubble Sort:
+
+Llamar a la función bubblesort para ordenar la lista desordenada.
+Intentar abrir (o crear) el archivo "lista_ordenada_bubble.txt" en modo de escritura.
+Para cada número en la lista ordenada, escribirlo en el archivo separado por una coma y un espacio.
+En caso de un error de archivo no encontrado, imprimir un mensaje de error.
+Implementación de Shell Sort:
+
+Definir una función llamada shellSort que tome una lista de números como argumento.
+Calcular la longitud de la lista y el tamaño inicial del espacio entre elementos.
+Utilizar el algoritmo Shell Sort para ordenar la lista.
+Imprimir la lista en cada iteración del algoritmo.
+Devolver la lista ordenada.
+Ordenar Lista Utilizando Shell Sort:
+
+Llamar a la función shellSort para ordenar la lista desordenada.
+Intentar abrir (o crear) el archivo "lista_ordenada_shellsort.txt" en modo de añadir.
+Para cada número en la lista ordenada, escribirlo en el archivo separado por una coma y un espacio.
+En caso de un error de archivo no encontrado, imprimir un mensaje de error.
+Ejecución Final:
+
+Llamar a la función que ordena la lista utilizando Bubble Sort.
+Llamar a la función que ordena la lista utilizando Shell Sort.
+Explicación:
+
+Se generan 10 números aleatorios únicos entre 1 y 10.
+La lista desordenada se guarda en un archivo llamado "listadesornedada.txt".
+Se implementa el algoritmo de Bubble Sort para ordenar la lista y se guarda en un nuevo archivo llamado "lista_ordenada_bubble.txt".
+Se implementa el algoritmo de Shell Sort para ordenar la lista y se guarda en un archivo existente llamado "lista_ordenada_shellsort.txt".
+En cada paso, se imprime la lista después de cada operación. En caso de errores de archivo, se manejan adecuadamente. """
 # Definición de la función "generar_numeros_aleatorios"
 def generar_numeros_aleatorios():
     import random
