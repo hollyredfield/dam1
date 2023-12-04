b8a1853345c045731b782b76f53f34e7773b21b8
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-12-04 21:24:02 +0100

    Update conexion.py

--- dam_python/biblioteca_pdd_mysql/conexion.py
+++ dam_python/biblioteca_pdd_mysql/conexion.py
@@ -1,28 +1,41 @@
-
-
+import mysql.connector
 class Conectar:
-        import mysql.connector 
+    def __init__(self):
+        self.conexion = None
 
-        class create_connection:
-            def __init__(self):
-                pass
+    def conexion_bdd(self):
+        try:    
+            self.conexion = mysql.connector.connect(
+                host='localhost',
+                user='root',
+                password='',
+                database='biblioteca'
+            )
+            print("Conexión exitosa.")
 
-            def conexion_bdd(self):
-                try:    
-                    conexion = mysql.connector.connect(
-                        host='localhost',
-                        user='root',
-                        password='',
-                        database='biblioteca'
-                    )
-                    print("Conexión exitosa.")
+        except mysql.connector.Error as e:
+            print("Error al conectar a la base de datos: ", e)
 
-                except mysql.connector.Error as e:
-                    print("Error al conectar a la base de datos: ", e)
+    def close_connection(self):
+        if self.conexion:
+            self.conexion.close()
+            print("Conexión cerrada.")
+    def add_book(self, titulo, autor, genero, paginas, anio_publicacion, edicion, idioma, ISBN, prestamos_realizados, prestado):
+        cursor = self.conexion.cursor()
+        add_book_query = ("INSERT INTO libros "
+                          "(titulo, autor, genero, paginas, anio_publicacion, edicion, idioma, ISBN, prestamos_realizados, prestado) "
+                          "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
+        book_data = (titulo, autor, genero, paginas, anio_publicacion, edicion, idioma, ISBN, prestamos_realizados, prestado)
+        cursor.execute(add_book_query, book_data)
+        self.conexion.commit()
+        cursor.close()
 
-            def cerrar_conexion(self, conexion_bdd):
-                try:
-                    conexion_bdd.close()
-                except mysql.connector.Error as e:
-                    print("Error al cerrar la conexión: " , e)
+        
+conect = Conectar()
+conect.conexion_bdd()
+conect.add_book("El señor de los anillos", "J.R.R. Tolkien", "Fantasia", 1000, 1954, 1, "Español", "123456789", 0, False)
+conect.close_connection()
 
+    # ... existing methods ...
+
+    
