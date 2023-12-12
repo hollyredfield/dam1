2a553507d64072f238b1e38b548448d7c78709a4
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-12-12 19:06:38 +0100

    save

---
+++ apuntes/crud_tabla_direccion/bdd.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+def conexion_bdd():
+    try:
+        return mysql.connector.connect(
+            host = "localhost",
+            user = "root",
+            password = "curso",
+            database = "empresa",
+        )
+    except mysql.connector.Error as z:
+        print("Error al conectar a la base de datos: ", z)
+def acabarconexion(conexion):
+    try:
+        conexion.close()
+    except mysql.connector.Error as z:
+        print("Error al tratar de cerar la conexión con la base de datos: ", z)
+        
---
+++ apuntes/crud_tabla_direccion/main.py
@@ -0,0 +1,10 @@
+import crud
+
+crud.crear("Avenida de Juan Carlos, 12")
+crud.crear("Torreón, 12")
+
+crud.leer_direccion()
+
+crud.actualizar_direccion(12," Juan Pablo Segundo")
+
+crud.eliminar_direccion(12)
