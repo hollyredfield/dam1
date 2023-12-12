372aa34558185fd8c0e0306bb9861500208d3550
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-12-12 18:56:03 +0100

    save

---
+++ apuntes/crud_cliente_departamento/bdd.py
@@ -0,0 +1,18 @@
+import mysql.connector
+def conexion_bdd():
+    try:
+        return mysql.connector.connect(
+            host = "localhost",
+            user = "root",
+            password = "",
+            database = "empresa"
+        )
+        
+    except mysql.connector.Error as z:
+        print("Error al conectar a la base de datos: ", z)
+        return None 
+def acabarconexion(conexion_bdd):   
+    try:
+        conexion_bdd.close()
+    except mysql.connector.Error as z:
+        print("Error al tratar de cerar la conexión con la base de datos: ", z)
---
+++ apuntes/crud_cliente_departamento/main.py
@@ -0,0 +1,11 @@
+from crud_clientes_departamentos import *
+from crud_clientes_departamentos import creardepartamento
+
+#ingresarempleados((1,"Juan",10001,1001))
+#eliminar_empleado((1,))
+#actualizar_empleado((1,"Juan",10001,1001,1))
+#leer_empleado()
+#creardepartamento(("Atención al cliente","Vallecas")) 
+#eliminar_departamento(("Atención al cliente",)) 
+#actualizar_departamento(("Atención al cliente","Vallecas","Zona sur"))
+#leer_departamento()
